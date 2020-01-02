1b4179b9fe5ec9d638d27eda55647ebfb9deea0c
Younes Bouddou <ybouddou@e1r11p5.1337.ma>
2020-01-03 00:54:57 +0100

    changes in x

--- Xx_main_ft.c
+++ Xx_main_ft.c
@@ -2,70 +2,90 @@
 
 int main()
 {
-	int i,j,y,x;
+	int i,j,y,x,n;
 
-	i = -5;
-	while (i < 5)
+	n = -7;
+	while (n < 7)
 	{
-		j = -5;
-		while (j < 5)
+		i = -5;
+		while (i < 5)
 		{
-			ft_printf("\n\n");
-			ft_printf("\033[0;33m[]\033[0m");
-			ft_printf("[%d]--[%d]\n", i, j);
-			x = ft_printf("|%*.*X|", i, j, -15);
-			ft_printf("\nreturn : %d\n", x);
-			j++;
+			j = -5;
+			while (j < 5)
+			{
+				ft_printf("\n\n");
+				ft_printf("\033[0;33m[]\033[0m");
+				ft_printf("[%d]--[%d]--[%d]\n", i, j, n);
+				x = ft_printf("|%*.*X|", i, j, n);
+				ft_printf("\nreturn : %d\n", x);
+				j++;
+			}
+			i++;
 		}
-		i++;
+		n++;
 	}
 
-	i = -5;
-	while (i < 5)
+	n = -7;
+	while (n < 7)
 	{
-		j = -5;
-		while (j < 5)
+		i = -5;
+		while (i < 5)
 		{
-			ft_printf("\n\n");
-			ft_printf("\033[0;33m[0]\033[0m");
-			ft_printf("[%d]--[%d]\n", i, j);
-			x = ft_printf("|%0*.*X|", i, j, -15);
-			ft_printf("\nreturn : %d\n", x);
-			j++;
+			j = -5;
+			while (j < 5)
+			{
+				ft_printf("\n\n");
+				ft_printf("\033[0;33m[0]\033[0m");
+				ft_printf("[%d]--[%d]--[%d]\n", i, j, n);
+				x = ft_printf("|%0*.*X|", i, j, n);
+				ft_printf("\nreturn : %d\n", x);
+				j++;
+			}
+			i++;
 		}
-		i++;
+		n++;
 	}
 
-	i = -5;
-	while (i < 5)
+	n = -7;
+	while (n < 7)
 	{
-		j = -5;
-		while (j < 5)
+		i = -5;
+		while (i < 5)
 		{
-			ft_printf("\n\n");
-			ft_printf("\033[0;33m[-]\033[0m");
-			ft_printf("[%d]--[%d]\n", i, j);
-			x = ft_printf("|%-*.*X|", i, j, -15);
-			ft_printf("\nreturn : %d\n", x);
-			j++;
+			j = -5;
+			while (j < 5)
+			{
+				ft_printf("\n\n");
+				ft_printf("\033[0;33m[-]\033[0m");
+				ft_printf("[%d]--[%d]--[%d]\n", i, j, n);
+				x = ft_printf("|%-*.*X|", i, j, n);
+				ft_printf("\nreturn : %d\n", x);
+				j++;
+			}
+			i++;
 		}
-		i++;
+		n++;
 	}
 
-	i = -5;
-	while (i < 5)
+	n = -7;
+	while (n < 7)
 	{
-		j = -5;
-		while (j < 5)
+		i = -5;
+		while (i < 5)
 		{
-			ft_printf("\n\n");
-			ft_printf("\033[0;33m[-0]\033[0m");
-			ft_printf("[%d]--[%d]\n", i, j);
-			x = ft_printf("|%-0*.*X|", i, j, -15);
-			ft_printf("\nreturn : %d\n", x);
-			j++;
+			j = -5;
+			while (j < 5)
+			{
+				ft_printf("\n\n");
+				ft_printf("\033[0;33m[-0]\033[0m");
+				ft_printf("[%d]--[%d]--[%d]\n", i, j, n);
+				x = ft_printf("|%-0*.*X|", i, j, n);
+				ft_printf("\nreturn : %d\n", x);
+				j++;
+			}
+			i++;
 		}
-		i++;
+		n++;
 	}
 
 	return (0);
